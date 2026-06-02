156c874a6d2d1287fb09155244060324f85ca9c0
CamBlackwell <cameronBlackwell80@gmail.com>
2026-06-02 12:58:57 +0930

    card format: added glass button, hover effects and setting button and global font variable

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -6,13 +6,13 @@ function PlantCard({ name, species, urgency }) {
           <h1 className="plant-card-title">{name}
             <p className="plant-card-species-bio">{species}</p>
           </h1>
-          <button className="plant-card-settings-button">settings </button>
+          <button className="plant-card-settings-button">...</button>
         </div>
 
         <img className="plant-image" src="/plant.jpg" alt="Plant" />
         <div className="plant-card-footer">
-          <p>water every {urgency}</p>
-          <button className="plant-card-water-button">water plant</button>
+          <h3>Water in: {urgency} Days</h3>
+          <button className="plant-card-water-button">WATER</button>
         </div>
       </div>
 
