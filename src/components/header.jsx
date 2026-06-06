a0478763c24e98b9ca447ba9fabe7f85b7da6489
CamBlackwell <cameronBlackwell80@gmail.com>
2026-06-06 15:44:48 +0930

    visual: added icons and buttons in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,23 @@
+import { CiCirclePlus, CiSettings } from "react-icons/ci";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import { FaList } from "react-icons/fa";
+import { PiPlant } from "react-icons/pi";
+
+
 function Header({ onOpenForm }) {
   return (
     <div className="header-box">
       <h1>WATER PLANT</h1>
 
-      <button> Info </button>
-      <button> Water Multiple </button>
+      <button> Info <IoIosInformationCircleOutline /></button>
+      <button> Water Multiple <FaList /></button>
+      <button> Edit Plants <PiPlant /></button>
+      <button> Settings <CiSettings /></button>
 
       <button className="Add-plant" onClick={() =>
         onOpenForm({})
       }
-      >Add Plant ⊕ </button>
+      >Add Plant <CiCirclePlus /> </button>
 
 
     </div>
